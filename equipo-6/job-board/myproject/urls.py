90e43d0418014b73df55295ed158dc6678557fa2
TheDeath <th3d34th777@gmail.com>
2025-10-15 14:53:01 -0400

    Eliminación del anterior

--- equipo-6/job-board/myproject/urls.py
+++
@@ -1,8 +0,0 @@
-"""myproject URL Configuration"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
-]
